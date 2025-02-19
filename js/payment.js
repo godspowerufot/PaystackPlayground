f18ae4461e2552249e50af5e6ecf26cbed7e7e16
godspowerufot <ufotgodspower8@gmail.com>
2025-02-19 11:05:56 +0100

    v6

--- js/payment.js
+++ js/payment.js
@@ -8,7 +8,7 @@ function payWithPaystack(e) {
   e.preventDefault();
 
   let handler = PaystackPop.setup({
-    key: PAYSTACK_KEY , // Replace with your public key
+    key:"pk_live_32fe5ff884e01fba885bb862920e91fec3e75bb4" , // Replace with your public key
     email: document.getElementById("email-address").value,
     amount: 100 * 100, // Convert NGN to kobo
     currency: 'NGN',
@@ -31,23 +31,3 @@ function closeModal() {
 }
 console.log(PAYSTACK_KEY);
 
-
-// Modal functionality
-const modal = document.getElementById('event-modal');
-const modalBtn = document.getElementById('modal-btn');
-const closeBtn = document.querySelector('.close');
-
-modalBtn.onclick = function() {
-    modal.style.display = 'flex';
-}
-
-closeBtn.onclick = function() {
-    modal.style.display = 'none';
-}
-
-// Close the modal if the user clicks outside of it
-window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.style.display = 'none';
-    }
-}
