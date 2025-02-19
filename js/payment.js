b4de86af6d33208d850de45b2952272b78e5fd03
godspowerufot <ufotgodspower8@gmail.com>
2025-02-19 09:24:20 +0100

    paystack

--- js/payment.js
+++ js/payment.js
@@ -2,11 +2,11 @@ const paymentForm = document.getElementById('submit');
 paymentForm.addEventListener("click", payWithPaystack, false);
 function payWithPaystack(e) {
   e.preventDefault();
-console.log("hi")
+// console.log(process.env.PK)
   let handler = PaystackPop.setup({
-    key: 'pk_test_67b30593c8f7f50f4dd367f564310a1f23eb37a1', // Replace with your public key
+    key: process.env.PK, // Replace with your public key
     email: document.getElementById("email-address").value,
-    amount: 1000 * 100, // Convert 1000 NGN to kobo (1000 * 100 = 100000)
+    amount: 100 * 100, // Convert 1000 NGN to kobo (1000 * 100 = 100000)
     currency: 'NGN',
     ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
     // label: "Optional string that replaces customer email"
